8c37053863394b7db7587e2832f80e744f5187f6
Dieote <gomezmdiego97@gmail.com>
2023-07-07 20:45:21 +0200

    cambio url configCORS

--- src/main/java/com/lt/config/ConfigCORs.java
+++ src/main/java/com/lt/config/ConfigCORs.java
@@ -10,7 +10,7 @@ public class ConfigCORs implements WebMvcConfigurer{
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
-                .allowedOriginPatterns("http://localhost:4200/","local-tatto-gm5epzr7q-dieote.vercel.app")
+                .allowedOriginPatterns("local-tatto-aqv3pnoc2-dieote.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true)
