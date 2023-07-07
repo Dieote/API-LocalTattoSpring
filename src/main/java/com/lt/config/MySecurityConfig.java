2750a4df5c55b1f6c2bbefde070068f5b3d81867
Dieote <gomezmdiego97@gmail.com>
2023-07-07 13:59:17 +0200

    password encoder y validacion de botones

--- src/main/java/com/lt/config/MySecurityConfig.java
+++ src/main/java/com/lt/config/MySecurityConfig.java
@@ -12,8 +12,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
@@ -37,8 +36,8 @@ public class MySecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Bean
-    public PasswordEncoder passwordEncoder(){
-        return NoOpPasswordEncoder.getInstance();
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 
     @Override
@@ -54,7 +53,7 @@ public class MySecurityConfig extends WebSecurityConfigurerAdapter {
                 .cors()
                 .disable()
                 .authorizeRequests()
-                .antMatchers("/generate-token", "/usuarios/", "/get-list-artists","/get-comments").permitAll()
+                .antMatchers("/generate-token", "/usuarios", "/get-list-artists","/get-comments", "/usuarios/post-user").permitAll()
                 .antMatchers((HttpMethod.OPTIONS)).permitAll()
                 .anyRequest().authenticated()
                 .and()
--- src/main/java/com/lt/controller/UserController.java
+++ src/main/java/com/lt/controller/UserController.java
@@ -6,6 +6,7 @@ import com.lt.domain.User;
 import com.lt.domain.UserRol;
 import com.lt.service.UsersService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Collections;
@@ -20,13 +21,18 @@ public class UserController {
     @Autowired
     private UsersService usersService;
 
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
     @PostMapping ("/post-user")
     public User guardarUsuario(@RequestBody User usuario) throws Exception{
+        usuario.setPassword(this.bCryptPasswordEncoder.encode(usuario.getPassword()));
+
         Set<UserRol> userroles = new HashSet<>();
 
         Rol rol = new Rol();
         rol.setRolId(2L);
-        rol.setNameRol("NORMAL");
+        rol.setNameRol("USER");
 
         UserRol usuarioRol = new UserRol();
         usuarioRol.setUser(usuario);
