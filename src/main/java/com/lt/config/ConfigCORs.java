c20c19d309386422a5fc01212a6f9fc9202691b6
Dieote <gomezmdiego97@gmail.com>
2023-07-07 20:05:44 +0200

    cambio url configCORS

--- src/main/java/com/lt/config/ConfigCORs.java
+++ src/main/java/com/lt/config/ConfigCORs.java
@@ -10,7 +10,7 @@ public class ConfigCORs implements WebMvcConfigurer{
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
-                .allowedOriginPatterns("http://localhost:4200")
+                .allowedOriginPatterns("http://localhost:4200/","https://local-tatto-git-master-dieote.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true)
