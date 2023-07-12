9788056d94b4c35395697d0ecf8b01d5f9a18c41
Dieote <gomezmdiego97@gmail.com>
2023-07-12 16:16:59 +0200

    Creacion de estructura para imagenes y archivos 2

---
+++ src/main/java/com/lt/FileNameException.java
@@ -0,0 +1,11 @@
+package com.lt;
+
+public class FileNameException extends RuntimeException {
+	public FileNameException(String message) {
+		super(message);
+	}
+
+	public FileNameException(String message, Throwable cause) {
+		super(message, cause);
+	}
+}
